f05fba9e4416a0175690f431ca405e3fe50e6715
daniel-dunsin <adejaredaniel12@gmail.com>
2023-04-14 04:00:37 +0100

    attemp to fix cors error

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(helmet());
 app.use(xss());
-app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(cookieParser());
 
 // Routes
